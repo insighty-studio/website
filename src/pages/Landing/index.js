8128a9fe1bbd5fd1bcd3ed2c79fad2402e57102e
JosepHawser <joseph.hawser@gmail.com>
2018-06-20 19:19:57 +0200

    NEW: Create structure for project

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './styles/landing.styl';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        Landing
+      </div>
+    );
+  }
+}
+
+export default Landing;
