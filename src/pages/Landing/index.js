28bb04ea22ff16676bcabbb700aa7dfa92f3cb10
Yevhenii Ivanisenko <yevhenii@Yevheniis-MacBook-Pro.local>
2018-06-29 13:21:25 +0300

    NEW: Added appearance/disappearance of displaying scroll button depending on current html area

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Scroll from 'react-scroll';
 import {ScrollToTopBtn} from 'imports/icons';
+import classnames from 'classnames';
 
 import Main from './sections/Main';
 import Services from './sections/Services';
@@ -11,9 +12,34 @@ import ContactUs from './sections/ContactUs';
 import './landing.styl';
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {percentagesScrolled: 0};
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const documentHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    );
+    const windowHeight = window.innerHeight;
+    const trackLength = documentHeight - windowHeight;
+    const scrollToTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    const percentagesScrolled = Math.floor(scrollToTop / trackLength * 100);
+    this.setState({percentagesScrolled});
+  }
+
   renderScrollBtn() {
     return (
-      <div className="scroll-btn" onClick={() => Scroll.animateScroll.scrollToTop({smooth: true, duration: 500})}>
+      <div
+        className={classnames('scroll-btn', this.state.percentagesScrolled >= 85 && 'scroll-btn-visible')}
+        onClick={() => Scroll.animateScroll.scrollToTop({smooth: true, duration: 500})}>
         <ScrollToTopBtn />
       </div>
     );
