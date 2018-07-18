16ca6057010941ce069eac328a5b9ca6f615accf
Yevhenii Ivanisenko <yevhenii@Yevheniis-MacBook-Pro.local>
2018-07-18 11:44:56 +0300

    IW-88 Remove unnecessary code

--- src/imports/components/ScrollUpButton/index.js
+++ src/imports/components/ScrollUpButton/index.js
@@ -15,23 +15,11 @@ class ScrollUpButton extends PureComponent {
     percentagesScrolled: 0
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      windowHeight: 0
-    };
-  }
-
-  componentDidMount() {
-    const windowHeight = window.innerHeight;
-    this.setState({windowHeight});
-  }
-
   render() {
     return (
       <div
         className={classnames('scroll-up-btn',
-          this.props.percentagesScrolled >= this.state.windowHeight && 'scroll-up-btn-visible')}
+          this.props.percentagesScrolled >= window.innerHeight && 'scroll-up-btn-visible')}
         onClick={() => Scroll.animateScroll.scrollToTop({smooth: true, duration: 500})}
       >
         <ScrollUpBtnIcon />
