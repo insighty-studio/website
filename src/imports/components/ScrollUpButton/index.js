c16c7279336677849f7fd63caa675bfaeccfc905
Yevhenii Ivanisenko <yevhenii@Yevheniis-MacBook-Pro.local>
2018-07-18 11:28:08 +0300

    IW-88 Implement scroll-up button appearance after 100vh passed

---
+++ src/imports/components/ScrollUpButton/index.js
@@ -0,0 +1,44 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import Scroll from 'react-scroll';
+import {ScrollUpBtnIcon} from 'icons';
+import classnames from 'classnames';
+
+import './index.styl';
+
+class ScrollUpButton extends PureComponent {
+  static propTypes = {
+    percentagesScrolled: PropTypes.number
+  };
+
+  static defaultProps = {
+    percentagesScrolled: 0
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowHeight: 0
+    };
+  }
+
+  componentDidMount() {
+    const windowHeight = window.innerHeight;
+    console.log(windowHeight);
+    this.setState({windowHeight});
+  }
+
+  render() {
+    return (
+      <div
+        className={classnames('scroll-up-btn',
+          this.props.percentagesScrolled >= this.state.windowHeight && 'scroll-up-btn-visible')}
+        onClick={() => Scroll.animateScroll.scrollToTop({smooth: true, duration: 500})}
+      >
+        <ScrollUpBtnIcon />
+      </div>
+    );
+  }
+}
+
+export default ScrollUpButton;
