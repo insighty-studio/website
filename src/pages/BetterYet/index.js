361462fcbf1cfa79b1996da39624f32337e27d54
igor-shyrnin <Shameless9701>
2018-07-20 20:05:14 +0300

    NEW: Improve BY page, Create ContactUsForm component

--- src/pages/BetterYet/index.js
+++ src/pages/BetterYet/index.js
@@ -1,10 +1,53 @@
 import React, {PureComponent} from 'react';
 
+import Header from './sections/Header';
+import About from './sections/About';
+import Prototypes from './sections/Prototypes';
+import Fonts from './sections/Fonts';
+import Colors from './sections/Colors';
+import ContactUs from './sections/ContactUs';
+import Footer from './sections/Footer';
+import Illustration from './sections/Illustration';
+
+import './index.styl';
+
 class BetterYet extends PureComponent {
+  renderTopPrototypes() {
+    return (
+      <Prototypes
+        prototypes={[
+          {src: require('./images/image1.png')},
+          {src: require('./images/image2.png')},
+          {src: require('./images/image3.png')},
+        ]}
+      />
+    );
+  }
+
+  renderBottomPrototypes() {
+    return (
+      <Prototypes
+        prototypes={[
+          {src: require('./images/image4.png')},
+          {src: require('./images/image5.png')},
+          {src: require('./images/image6.png')},
+        ]}
+      />
+    );
+  }
+
   render() {
     return (
       <div className="betteryet-page">
-        BetterYet Page
+        <Header />
+        <About />
+        {this.renderTopPrototypes()}
+        <Fonts />
+        {this.renderBottomPrototypes()}
+        <Colors />
+        <Illustration />
+        <ContactUs />
+        <Footer />
       </div>
     );
   }
