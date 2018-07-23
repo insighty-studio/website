7590c7cfa3bb4c1c98ee40bd33f9cd3be8296ab5
igor-shyrnin <Shameless9701>
2018-07-23 19:00:40 +0300

    FIX: Fix ESLint issues

--- src/pages/Loader/LoaderLine/index.js
+++ src/pages/Loader/LoaderLine/index.js
@@ -1,9 +1,18 @@
 import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 import {InsightyLogo} from 'icons';
 
 import './index.styl';
 
 class LoaderLine extends PureComponent {
+  static propTypes = {
+    loading: PropTypes.number
+  };
+
+  static defaultProps = {
+    loading: 0
+  };
+
   render() {
     return (
       <div className="loader-page">
