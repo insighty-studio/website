2116eb63a17bad6ae0c0de044bdd335677769318
igor-shyrnin <Shameless9701>
2018-07-27 18:46:07 +0300

    FIX Eslint error

--- src/imports/components/Sections/WhitePaper/index.js
+++ src/imports/components/Sections/WhitePaper/index.js
@@ -1,8 +1,17 @@
 import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 
 import './index.styl';
 
 class WhitePaper extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node
+  };
+
+  static defaultProps = {
+    children: ''
+  };
+
   render() {
     const {children} = this.props;
 
