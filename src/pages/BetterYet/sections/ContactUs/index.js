678f39970b97b269dbb7e25f0147c7c64c88f187
Yevhenii Ivanisenko <yevhenii@Yevheniis-MacBook-Pro.local>
2018-07-28 21:03:16 +0300

    Make clients section mobile responsive on landing, betteryet pages. Fix some styling according to the latest design.

--- src/pages/BetterYet/sections/ContactUs/index.js
+++ src/pages/BetterYet/sections/ContactUs/index.js
@@ -1,6 +1,8 @@
 import React, {PureComponent} from 'react';
 import ContactUsForm from 'components/ContactUsForm';
 
+import './index.styl';
+
 class ContactUs extends PureComponent {
   render() {
     return <ContactUsForm />;
