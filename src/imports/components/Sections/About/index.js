50d591bdb50f139eba8498fb5751318ab0aa7f49
Yevhenii Ivanisenko <yevhenii@Yevheniis-MacBook-Pro.local>
2018-08-03 19:46:40 +0300

    Resolve conflicts

--- src/imports/components/Sections/About/index.js
+++ src/imports/components/Sections/About/index.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import Description from 'components/BigDescription';
+import Description from 'components/Description';
 import {Parallax} from 'react-scroll-parallax';
 
 import AboutBackground from './components/AboutBackground';
