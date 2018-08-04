4a9bcea9b2808df8ebc2971fccdab4575ba925fa
Yevhenii Ivanisenko <yevhenii@Yevheniis-MacBook-Pro.local>
2018-08-04 15:55:11 +0300

    Fix Landing header cloud background svg

--- src/pages/Landing/sections/Header/components/Illustration/index.js
+++ src/pages/Landing/sections/Header/components/Illustration/index.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 import {Parallax} from 'react-scroll-parallax';
 
-import CloudIcon from './cloud';
+import CloudIcon from './cloud.js';
 import './index.styl';
 
 class Illustration extends PureComponent {
