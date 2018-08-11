99d9e048f5397c959bd1d4805cbe6babfd333d84
igor-shyrnin <Shameless9701>
2018-08-11 17:39:36 +0300

    NEW: Create hamburger for mobile

--- src/imports/components/NavigationBar/index.js
+++ src/imports/components/NavigationBar/index.js
@@ -1,7 +1,18 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import {Link} from 'react-scroll';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+
+import {
+  MenuIcon,
+  CloseMenuIcon,
+  LinkedInIcon,
+  FacebookIcon,
+  ClutchIcon,
+  AngelListIcon,
+  BloggerIcon
+} from './icons';
 
 import './index.styl';
 import './mobile/index.styl';
@@ -17,49 +28,108 @@ class NavigationBar extends React.Component {
     white: false
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuStatus: ''
+    };
+  }
+
+  clearMenuStatus(i, duration) {
+    if (i === 0) this.setState({isMenuStatus: ''});
+    setTimeout(() => this.setState({isMenuStatus: ''}), duration);
+  }
+
   renderLinks() {
     const {links, white} = this.props;
-    return links.map(({
-      text, to, href, active
-    }, i) => {
-      if (href) {
-        return (
-          <a
-            key={i}
-            href={href}
-            className={white && 'white'}
-            rel="noopener noreferrer"
-          >
-            {text}
-          </a>
-        );
-      }
-      return (
-        <Link
-          smooth
-          key={i}
-          to={to}
-          activeClass="active"
-          duration={500 + (i * 200)}
-          className={classnames({active}, white && 'white')}
-        >
-          {text}
-        </Link>
-      );
-    });
+    return (
+      <div className="links">
+        {links.map(({
+          text, to, href, active
+        }, i) => {
+          const duration = 500 + (i * 200);
+          if (href) {
+            return (
+              <a
+                key={i}
+                href={href}
+                onClick={() => this.clearMenuStatus(i, duration)}
+                className={white && 'white'}
+                rel="noopener noreferrer"
+              >
+                {text}
+              </a>
+            );
+          }
+          return (
+            <Link
+              smooth
+              key={i}
+              to={to}
+              activeClass="active"
+              onClick={() => this.clearMenuStatus(i, duration)}
+              duration={duration}
+              className={classnames({active}, white && 'white')}
+            >
+              {text}
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
+
+  renderOpenedMenu() {
+    return (
+      <div
+        className="close-menu-icon"
+        onClick={() => this.setState({isMenuStatus: ''})}
+      >
+        <CloseMenuIcon />
+      </div>
+    );
+  }
+
+  renderClosedMenu() {
+    return (
+      <div
+        className="menu-icon"
+        onClick={() => this.setState({isMenuStatus: 'opened'})}
+      >
+        <MenuIcon />
+      </div>
+    );
+  }
+
+  renderSocial() {
+    return (
+      <div className="social-links">
+        <FacebookIcon />
+        <LinkedInIcon />
+        <BloggerIcon />
+        <ClutchIcon />
+        <AngelListIcon />
+      </div>
+    );
   }
 
   render() {
     const {logoIcon} = this.props;
+    const {isMenuStatus} = this.state;
+
     return (
-      <div className="navigation-bar">
+      <div className={classnames('navigation-bar', isMenuStatus)}>
         <div className="bar-content">
-          <a href="/" rel="noopener noreferrer">
-            {logoIcon}
-          </a>
-          <div className="links">
-            {this.renderLinks()}
+          <div className="top-bar">
+            <a className="logo-icon" href="/" rel="noopener noreferrer">
+              {logoIcon}
+            </a>
+            <div className="menu">
+              {isMenuStatus ? this.renderOpenedMenu() : this.renderClosedMenu()}
+            </div>
           </div>
+          {this.renderLinks()}
+          {isMenuStatus && this.renderSocial()}
         </div>
       </div>
     );
