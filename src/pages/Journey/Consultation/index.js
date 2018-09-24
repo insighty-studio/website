19f61cef1b299fa4b156661f30f2e947ca116fb4
Denys Halenok <denys.halenok@gmail.com>
2018-09-24 13:12:19 +0300

    Implement a mobile version of Consultation page

--- src/pages/Journey/Consultation/index.js
+++ src/pages/Journey/Consultation/index.js
@@ -7,6 +7,7 @@ import Input from 'components/Input';
 import TextArea from 'components/TextArea';
 
 import './index.styl';
+import './mobile/index.styl';
 
 class Consultation extends Component {
   render() {
@@ -19,6 +20,14 @@ class Consultation extends Component {
 
         <div className="consult-content">
           <Heading className="consult-heading">Fancy A Free 30-Minute Product Development Session?</Heading>
+          <div className="consult-text-mobile">
+            <p className="consult-para">Here’s the plan:</p>
+            <p className="consult-para">- We learn about your idea.</p>
+            <p className="consult-para">
+              - We outline the community of people that you want to reach with your product.
+            </p>
+            <p className="consult-para">- You get your idea visualized via mockups a couple of days after.</p>
+          </div>
           <p className="consult-para">Here’s the plan:</p>
           <p className="consult-para">- We learn as much as we can about your idea.</p>
           <p className="consult-para">- We outline the community of people that you want to reach with your product.</p>
